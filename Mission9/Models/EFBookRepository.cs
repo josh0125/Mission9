caf75852d2a393131dbbeb55e87b718b74849362
Joshua Allen <joshuatallen99@gmail.com>
2023-03-01 17:27:23 -0700

    Add project files.

---
+++ Mission9/Models/EFBookRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission9.Models
+{
+    public class EFBookRepository : iBookRepository
+    {
+        public BookStoreContext context { get; set; }
+
+        public EFBookRepository (BookStoreContext temp)
+        {
+            context = temp;
+        }
+        public IQueryable<Book> Books => context.Books;
+    }
+}
